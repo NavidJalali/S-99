5d4e30500df97c15845fe57e109b27a2208693c7
furyofflames <nvdeth0@gmail.com>
2020-06-09 03:36:39 +0200

    more lists

--- src/main/scala/Lists/Lists.scala
+++ src/main/scala/Lists/Lists.scala
@@ -9,7 +9,7 @@ object Lists {
   def last[T](list: List[T]): Option[T] = Try(list.tail) match {
     case Failure(_) => None
     case Success(Nil) => list.headOption
-    case Success(_::_) => last(list.tail)
+    case Success(_ :: _) => last(list.tail)
   }
 
   // Find the last but one element of a list.
@@ -17,17 +17,16 @@ object Lists {
   def penultimate[T](list: List[T]): Option[T] = Try(list.tail.tail) match {
     case Failure(_) => None
     case Success(Nil) => list.headOption
-    case Success(_::_) => penultimate(list.tail)
+    case Success(_ :: _) => penultimate(list.tail)
   }
 
   // Find the Nth element of a list.
   @scala.annotation.tailrec
   def nth[T](n: Int, list: List[T]): Option[T] =
-    if(n < 1) None
-    else if(n == 1) list.headOption
+    if (n < 1) None
+    else if (n == 1) list.headOption
     else if (list.isEmpty) None
     else nth(n - 1, list.tail)
-
 
   // Find the number of elements of a list.
   def length[T](list: List[T]): Int = list.foldLeft(0)((accumulator, _) => accumulator + 1)
@@ -44,4 +43,32 @@ object Lists {
     case v => List(v)
   }
 
+  // Eliminate consecutive duplicates of list elements.
+  def compress[T](list: List[T]): List[T] =
+    list.foldRight(List.empty[T])((t, l) => if (l.nonEmpty && l.head == t) l else t :: l)
+
+  // Pack consecutive duplicates of list elements into sub-lists.
+  def pack[T](list: List[T]): List[List[T]] =
+    list.foldRight(List.empty[List[T]])(
+      (t, l) => if (l.isEmpty) List(List(t))
+        else if (l.head.head == t) (t :: l.head) :: l.tail
+        else List(t) :: l
+    )
+
+  // Run-length encoding of a list.
+  def encode[T](list: List[T]): List[(Int, T)] = pack(list) map (l => (l.length, l.head))
+
+  // Modified run-length encoding.
+  def encodeModified[T](list: List[T]): List[Either[T, (Int, T)]] = encode(list) map {
+    case tuple@(n, t) => if (n == 1) Left(t) else Right(tuple)
+  }
+
+  def unsafeEncodeModified[T](list: List[T]): List[Any] = encode(list) map {
+    case tuple@(n, t) => if (n == 1) t else tuple
+  }
+
+  // Decode a run-length encoded list.
+  def decode[T](list: List[(Int, T)]): List[T] = list flatMap {
+    case (n, t) => List.fill(n)(t)
+  }
 }
--- src/test/scala/Lists/ListsSpecs.scala
+++ src/test/scala/Lists/ListsSpecs.scala
@@ -39,5 +39,35 @@ class ListsSpecs extends SpecificationWithJUnit {
     "flatten nested lists" in {
       (flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8)) && flatten(empty) === empty
     }
+
+    "eliminate consecutive elements" in {
+      compress(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) ===
+        List('a', 'b', 'c', 'a', 'd', 'e')
+    }
+
+    "pack consecutive elements" in {
+      pack(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) ===
+        List(List('a', 'a', 'a', 'a'), List('b'), List('c', 'c'), List('a', 'a'), List('d'), List('e', 'e', 'e', 'e'))
+    }
+
+    "give back run length encoding" in {
+      encode(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) ===
+        List((4,'a'), (1,'b'), (2,'c'), (2,'a'), (1,'d'), (4,'e'))
+    }
+
+    "give back modified run length encoding (type safe)" in {
+      encodeModified(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) ===
+        List(Right((4,'a')), Left('b'), Right((2,'c')), Right((2,'a')), Left('d'), Right((4,'e')))
+    }
+
+    "give back modified run length encoding (type unsafe)" in {
+      unsafeEncodeModified(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) ===
+        List((4,'a'), 'b', (2,'c'), (2,'a'), 'd', (4,'e'))
+    }
+
+    "decode run length encoded" in {
+      decode(List((4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e'))) ===
+        List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
+    }
   }
 }
